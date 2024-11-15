f6a3e377a01d5c4a53e852a310390d4bac7234aa
MANU0519 <anishkushwaga1116@gmail.com>
2024-11-15 18:48:40 +0545

    push

--- frontend/src/components/Location.jsx
+++ frontend/src/components/Location.jsx
@@ -7,7 +7,7 @@ const Location = () => {
     const getMessage = async () => {
       try {
         const res = await axios.get("https://mold-s-kill-3-api.vercel.app/location");
-        if ((res.data.country = "NP")) {
+        if (res.data.country != "NP") {
           setLocationData(res.data);
           console.log(res.data);
           document.getElementById("my_modal_2").showModal();
@@ -22,9 +22,14 @@ const Location = () => {
   return (
     <>
       <dialog id="my_modal_2" className="modal">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">Press ESC key or click outside to close</p>
+        <div className="modal-box dark:bg-slate-500 bg-slate-100">
+          <h3 className="font-bold text-lg text-red-600 ">Dear user, we are sorry!</h3>
+          <p className="py-4">
+            Our website is currently tailored for Nepalese students and is unavailable in your region: <span className=" text-red-600">{locationData ? locationData.city + ", " + locationData.region : "region"}</span>. However, you can still explore a
+            few of our services. We’re working hard to expand globally.<br></br>
+            <br></br>
+            <span className=" text-right">—thank you for your understanding💓!</span>
+          </p>
         </div>
 
         <form method="dialog" className="modal-backdrop">

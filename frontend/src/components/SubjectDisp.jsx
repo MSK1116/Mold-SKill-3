ecc84f71d24c1c1c818e59c4e22347d2556094e1
MANU0519 <anishkushwaha1116@gmail.com>
2025-03-27 07:01:19 +0545

    Remove console log from SubjectDisp component

--- frontend/src/components/SubjectDisp.jsx
+++ frontend/src/components/SubjectDisp.jsx
@@ -3,7 +3,7 @@ import Cards1 from "./Cards1";
 import list_org from "../../public/sub_list.json";
 const SubjectDisp = () => {
   const filterData = list_org.filter((data) => data.category !== "Free");
-  console.log(filterData);
+
   const shuffleArray = (array) => {
     return array.slice().sort(() => Math.random() - 0.5);
   };
